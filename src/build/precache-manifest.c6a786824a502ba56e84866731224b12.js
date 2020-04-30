71b38df700895bd387618cd0ef575178aaf46f5c
Patrick Trillo <patarstar7@patricks-mbp.lan>
2020-04-30 16:30:04 -0700

    FIRST COMMIT FUCK YA

---
+++ src/build/precache-manifest.c6a786824a502ba56e84866731224b12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c2a0413fac44ddb3615924ee1f53e8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad9beaed7a4b9fea5f57",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "0fa31cfaa0a7d89ba0a5",
+    "url": "/static/js/2.a32e5dd8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a32e5dd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad9beaed7a4b9fea5f57",
+    "url": "/static/js/main.f905e04a.chunk.js"
+  },
+  {
+    "revision": "c6cd80a6fce542ddaee9",
+    "url": "/static/js/runtime-main.b9244b71.js"
+  }
+]);
